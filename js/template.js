101428b0f87fdfd3badb8df93efc51d5b32a8210
Arthur Ribeiro <148250310+arthurrsn@users.noreply.github.com>
2024-02-22 08:33:53 -0300

    Finalizando header

--- js/template.js
+++ js/template.js
@@ -1,4 +1,4 @@
-// Adiciona um ouvinte de evento para o ícone do menu móvel
+// Adiciona um ouvinte de evento para o ícone do menu móvel.
 document.querySelector('.menu-icon').addEventListener('click', function() {
   // Alterna a visibilidade da navegação de categorias móveis
   document.querySelector('.mobile-categories').classList.toggle('show');
