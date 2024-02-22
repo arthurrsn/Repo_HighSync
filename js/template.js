f3c5622ab1dfeaea88c36699f19d816809dbdb48
Arthur Ribeiro <148250310+arthurrsn@users.noreply.github.com>
2024-02-22 08:30:20 -0300

    header finalizado

--- js/template.js
+++ js/template.js
@@ -1,6 +1,5 @@
 // Adiciona um ouvinte de evento para o ícone do menu móvel
 document.querySelector('.menu-icon').addEventListener('click', function() {
-    // Alterna a visibilidade da navegação de categorias móveis
-    document.querySelector('.mobile-categories').classList.toggle('show');
-  });
-  
+  // Alterna a visibilidade da navegação de categorias móveis
+  document.querySelector('.mobile-categories').classList.toggle('show');
+});
